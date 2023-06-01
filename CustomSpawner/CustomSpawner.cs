2e0e7fadd3fe73b0647d634d0e61c4f73ee36696
moddedmcplayer <76910334+moddedmcplayer@users.noreply.github.com>
2023-06-01 16:22:34 +0200

    bump ver

--- CustomSpawner/CustomSpawner.cs
+++ CustomSpawner/CustomSpawner.cs
@@ -17,7 +17,7 @@ namespace CustomSpawner
 		public override string Author { get; } = "Kognity";
 		public override string Prefix { get; } = "CustomSpawner";
 		public override Version RequiredExiledVersion { get; } = new Version(5, 2, 0);
-		public override Version Version { get; } = new Version(2, 0, 0);
+		public override Version Version { get; } = new Version(2, 2, 0);
 
 		public override void OnEnabled()
 		{
